9b6844b9c5484405d4db8ff99b4dc1cf1cdb3225
Andreias Zota <andreias.zota@live.at>
2022-06-22 19:03:11 +0200

    init commit

---
+++ src/main/java/com/example/demo/DateTimeController.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import java.sql.Time;
+import java.time.LocalTime;
+import java.util.Date;
+
+public class DateTime {
+
+
+    public LocalTime GetLocalTime(){
+        LocalTime time = LocalTime.now();
+
+        return time;
+    }
+
+}
